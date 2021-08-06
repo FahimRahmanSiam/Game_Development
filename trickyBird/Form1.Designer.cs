0596b15e5936791f1c8a975b919702eae0f40c62
Fahim Rahman <fahimrahman.siam>
2021-08-06 20:15:03 +0600

    first commit

---
+++ trickyBird/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace trickyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.gameScore = new System.Windows.Forms.Label();
+            this.pipeUp = new System.Windows.Forms.PictureBox();
+            this.pipeDown = new System.Windows.Forms.PictureBox();
+            this.weather = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeUp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.weather)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bird
+            // 
+            this.bird.BackColor = System.Drawing.Color.Transparent;
+            this.bird.Image = ((System.Drawing.Image)(resources.GetObject("bird.Image")));
+            this.bird.Location = new System.Drawing.Point(40, 143);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(57, 56);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 0;
+            this.bird.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = ((System.Drawing.Image)(resources.GetObject("ground.Image")));
+            this.ground.Location = new System.Drawing.Point(-4, 379);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(810, 79);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 1;
+            this.ground.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // gameScore
+            // 
+            this.gameScore.AutoSize = true;
+            this.gameScore.BackColor = System.Drawing.Color.Gray;
+            this.gameScore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gameScore.Font = new System.Drawing.Font("Segoe MDL2 Assets", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameScore.Location = new System.Drawing.Point(12, 409);
+            this.gameScore.Name = "gameScore";
+            this.gameScore.Padding = new System.Windows.Forms.Padding(0, 0, 0, 7);
+            this.gameScore.Size = new System.Drawing.Size(115, 39);
+            this.gameScore.TabIndex = 2;
+            this.gameScore.Text = "Score: 0";
+            // 
+            // pipeUp
+            // 
+            this.pipeUp.BackColor = System.Drawing.Color.Transparent;
+            this.pipeUp.Image = ((System.Drawing.Image)(resources.GetObject("pipeUp.Image")));
+            this.pipeUp.Location = new System.Drawing.Point(642, -2);
+            this.pipeUp.Name = "pipeUp";
+            this.pipeUp.Size = new System.Drawing.Size(100, 135);
+            this.pipeUp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeUp.TabIndex = 3;
+            this.pipeUp.TabStop = false;
+            // 
+            // pipeDown
+            // 
+            this.pipeDown.BackColor = System.Drawing.Color.Transparent;
+            this.pipeDown.Image = ((System.Drawing.Image)(resources.GetObject("pipeDown.Image")));
+            this.pipeDown.Location = new System.Drawing.Point(478, 262);
+            this.pipeDown.Name = "pipeDown";
+            this.pipeDown.Size = new System.Drawing.Size(100, 121);
+            this.pipeDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeDown.TabIndex = 4;
+            this.pipeDown.TabStop = false;
+            // 
+            // weather
+            // 
+            this.weather.BackColor = System.Drawing.Color.Transparent;
+            this.weather.Image = ((System.Drawing.Image)(resources.GetObject("weather.Image")));
+            this.weather.Location = new System.Drawing.Point(456, 12);
+            this.weather.Name = "weather";
+            this.weather.Size = new System.Drawing.Size(99, 77);
+            this.weather.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.weather.TabIndex = 5;
+            this.weather.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ClientSize = new System.Drawing.Size(766, 450);
+            this.Controls.Add(this.weather);
+            this.Controls.Add(this.pipeDown);
+            this.Controls.Add(this.pipeUp);
+            this.Controls.Add(this.gameScore);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.bird);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tricky Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeUp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.weather)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label gameScore;
+        private System.Windows.Forms.PictureBox pipeUp;
+        private System.Windows.Forms.PictureBox pipeDown;
+        private System.Windows.Forms.PictureBox weather;
+    }
+}
+
---
+++ trickyBird/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trickyBird
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int gravity = 10;
+        int speed = 25;
+        int score = 0;
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+                gravity = 25;
+            
+        } 
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+                gravity = -25;
+            
+            else if(e.KeyCode == Keys.Enter)
+                timer1.Start();
+            
+        }
+        Random rnd = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bird.Top += gravity;
+            pipeDown.Left -= speed;
+            pipeUp.Left -= speed;
+            weather.Left -= speed;
+            gameScore.Text = $"Score: {score}";
+            if (pipeDown.Left < -170)
+            {
+                pipeDown.Left = rnd.Next(300, 600);
+                score++;
+            }
+            if (pipeUp.Left < -170)
+            {
+                int top= rnd.Next(300, 700);
+                pipeUp.Left = top;
+                weather.Left = rnd.Next(top + 50, 770);
+                score++;
+            }
+            if(bird.Bounds.IntersectsWith(pipeDown.Bounds) || bird.Bounds.IntersectsWith(pipeUp.Bounds) || bird.Bounds.IntersectsWith(ground.Bounds))
+            {
+                timer1.Stop();
+                gameScore.Text += "  Gameover!!!";
+            }
+            if (score > 5)
+            {             
+                speed += 2;
+            }
+            if (score > 10)
+            {
+                speed += 2;
+            }
+            if (score > 15)
+            {
+                speed += 2;
+            }
+            if (score > 25)
+            {
+                speed += 2;
+            }
+        }
+    }
+}
